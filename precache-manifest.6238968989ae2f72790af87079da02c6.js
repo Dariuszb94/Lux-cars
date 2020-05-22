b0e13388401f0fa9ca7b155e18c798025872d9fb
dariuszb <greedo904@gmail.com>
2020-05-22 13:11:05 +0200

    Updates

---
+++ precache-manifest.6238968989ae2f72790af87079da02c6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65f85d11a12df18ffc8e7ed3f175475e",
+    "url": "/Lux-cars/index.html"
+  },
+  {
+    "revision": "d1d68f134436bedd9a07",
+    "url": "/Lux-cars/static/css/main.af5dee58.chunk.css"
+  },
+  {
+    "revision": "72a19dada963a59d344d",
+    "url": "/Lux-cars/static/js/2.3dcea323.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Lux-cars/static/js/2.3dcea323.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1d68f134436bedd9a07",
+    "url": "/Lux-cars/static/js/main.8a898143.chunk.js"
+  },
+  {
+    "revision": "84ff2a4d21bed614454f",
+    "url": "/Lux-cars/static/js/runtime-main.e82becdc.js"
+  },
+  {
+    "revision": "6da71c8bbc740af220b69007330c7535",
+    "url": "/Lux-cars/static/media/Logo.6da71c8b.png"
+  },
+  {
+    "revision": "2a563a0ea72c5a338c793191c7b71c8b",
+    "url": "/Lux-cars/static/media/slide1bg.2a563a0e.jpg"
+  },
+  {
+    "revision": "05d6c50409da1bc63941a0a032971d73",
+    "url": "/Lux-cars/static/media/slide2bg.05d6c504.jpg"
+  }
+]);
